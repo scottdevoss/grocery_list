a89acdd18e03a5c74f7cd6d75765bc9d1a10f711
Scott DeVoss <scottdevoss@yahoo.com>
2024-04-04 09:21:39 -0600

    added delete test

--- spec/features/grocery_list/index_spec.rb
+++ spec/features/grocery_list/index_spec.rb
@@ -1,16 +1,33 @@
 require "rails_helper"
 
 RSpec.describe "Grocery List" do
-  it "Creates a grocery list" do
-    visit "/add_item"
-    
-    expect(page).to have_content("Grocery List")
+  describe "Creating a grocery list" do
+    it "Adds items to a grocery list" do
+      visit "/add_item"
+      
+      expect(page).to have_content("Grocery List")
 
-    fill_in :item, with: "Bananas"
-    click_button "Add"
+      fill_in :item, with: "Bananas"
 
-    expect(current_path).to eq("/grocery_list")
+      click_button "Add"
 
-    expect(page).to have_content("Bananas")
+      expect(current_path).to eq("/grocery_list")
+      expect(page).to have_content("Bananas")
+    end
+
+    it "Deletes an item from the list" do
+      visit "/add_item"
+
+      fill_in :item, with: "Onions"
+
+      click_button "Add"
+
+      expect(page).to have_content("Onions")
+      expect(page).to have_button("Delete")
+
+      click_button "Delete"
+
+      expect(page).to_not have_content("Onions")
+    end
   end
 end
