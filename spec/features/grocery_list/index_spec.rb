e97f93dc5682d70891caa5d44a2a6068b3b9c2da
Scott DeVoss <scottdevoss@yahoo.com>
2024-04-04 10:42:49 -0600

    added update test

--- spec/features/grocery_list/index_spec.rb
+++ spec/features/grocery_list/index_spec.rb
@@ -22,12 +22,35 @@ RSpec.describe "Grocery List" do
 
       expect(page).to have_content("Onions")
 
-      expect(page).to have_button("Delete")
+      expect(page).to have_button("Delete Onions")
 
       click_button "Delete Onions"
       
       expect(current_path).to eq("/grocery_list")
       
+      expect(page).to_not have_content("Onions")
+    end
+
+    it "Updates an item from the list" do
+      item1 = Item.create!(name: "Onions")
+
+      visit "/grocery_list"
+
+      expect(page).to have_content("Onions")
+
+      expect(page).to have_button("Update Onions")
+
+      click_button "Update Onions"
+
+      expect(current_path).to eq("/update")
+
+      fill_in :name, with: "Peaches"
+
+      click_button "Update"
+
+      expect(current_path).to eq("/grocery_list")
+
+      expect(page).to have_content("Peaches")
       expect(page).to_not have_content("Onions")
     end
   end
